11746a3fac8897f9f59ee7eeee9f6fba8bef0743
not-so-good <sanjin.suljanovic@gmail.com>
2021-04-01 21:00:15 +0200

    ex01 Fetch - GET simple request

--- ex01/src/GetRequest.js
+++ ex01/src/GetRequest.js
@@ -7,12 +7,13 @@ class GetRequest extends React.Component {
   }
 
   componentDidMount() {
-    fetch("api.npms.io/v2/search?q=react")
+    fetch("https://api.npms.io/v2/search?q=react")
       .then((response) => response.json())
       .then((data) => this.setState({ totalReactPackages: data.total }));
   }
 
   render() {
+    const { totalReactPackages } = this.state;
     return (
       <div className="card text-center m-3">
         <h5 className="card-header">Simple GET Request</h5>
