50d7450227c37dd857f88680ba8c399ee9b7fc0e
not-so-good <sanjin.suljanovic@gmail.com>
2021-04-01 21:33:01 +0200

    ex08 Fetch - POST with headers

---
+++ ex08/src/App.js
@@ -0,0 +1,11 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import PostRequestSetHeaders from "./PostRequestSetHeaders";
+function App() {
+  return (
+    <div className="App">
+      <PostRequestSetHeaders />
+    </div>
+  );
+}
+export default App;
