0eb100f93e7e4d96e2eff74aa45b398042128e50
not-so-good <sanjin.suljanovic@gmail.com>
2021-04-01 21:29:53 +0200

    ex07 Fetch - POST error handling

---
+++ ex07/src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import "./App.css";
+import PostRequestErrorHandling from "./PostRequestErrorHandling";
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestErrorHandling />
+    </div>
+  );
+}
+export default App;
